31ad6d7a0bf7143d140b0312baa11b28e55e6759
root <root@curso-postgresql.dexter.com.br>
2017-10-01 16:47:04 -0300

    Upload Files Swarm Docker-compose DigitalOcean

---
+++ Swarm_Compose_DO/create_keystore_DO.sh
@@ -0,0 +1,2 @@
+
+docker-machine create --driver digitalocean --digitalocean-access-token 3ebf6ff144ea399314b8fa10a649cf1b0abaca187a26811848c309aaa0704206 --digitalocean-private-networking dexter-keystore
